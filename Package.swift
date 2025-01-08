2d9ca5cdf5f0612dcd3b61714d6cb24866bd9445
sq-0523 <sq-0523>
2025-01-08 17:24:30 +0800

    QVReplayKit

--- Package.swift
+++ Package.swift
@@ -9,13 +9,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "QVReplayKitExt",
-            targets: ["QVReplayKitExt"]),
+            targets: ["QVReplayKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "QVReplayKitExt"),
+        .binaryTarget(name: "QVReplayKit", path: "QVReplayKit.xcframework"),
+
 
     ]
 )
